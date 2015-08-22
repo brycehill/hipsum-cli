d2c6b6263bc415afe5a5c6b707be3611e6d76fd9
Bryce Hill <bryceahill@gmail.com>
2015-08-21 20:30:55 -0700

    Initial dep and script commit

---
+++ bin/hipsum.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+var words = require('hipsteripsum')
+var chalk = require('chalk').cyan
+var buffer = require('copy-paste')
+
+var ps = process.argv.slice(2)[0] || 2
+var text = words.get(ps)
+buffer.copy(text, function() {
+    var p = 'paragraph' + (ps > 1 ? 's' : '')
+    var msg = 'Copied '+ [ps, p].join(' ') +' of hipstery goodness'
+    console.log(chalk(msg))
+})
+
