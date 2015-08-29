03e53784c508c6a5b1e11402b9cc9b62de497643
Bryce Hill <bryceahill@gmail.com>
2015-08-29 15:26:40 -0700

    Only produce hipster text

--- bin/hipsum.js
+++ bin/hipsum.js
@@ -5,7 +5,7 @@ var chalk = require('chalk').cyan
 var buffer = require('copy-paste')
 
 var ps = process.argv.slice(2)[0] || 2
-var text = words.get(ps)
+var text = words.get(ps, false, true)
 buffer.copy(text, function() {
     var p = 'paragraph' + (ps > 1 ? 's' : '')
     var msg = 'Copied '+ [ps, p].join(' ') +' of hipstery goodness'
